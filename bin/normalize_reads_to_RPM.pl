20ab35b2e1322bf52f1de315d574c918db1e47ce
Scott A. Givan <sgivan@yahoo.com>
2012-07-11 17:00:16 -0500

    Added dbhost and dbport options.

--- bin/normalize_reads_to_RPM.pl
+++ bin/normalize_reads_to_RPM.pl
@@ -1,13 +1,16 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 #
 use strict;
 use Getopt::Long;
 use File::Spec;
 
 my ($user,$password,$database,$fsource,$debug,$help,$justsummary,$verbose,$install_path);
+my ($dbhost,$dbport);
 
 GetOptions(
     "user=s"        =>  \$user,
+    "dbhost=s"      =>  \$dbhost,
+    "dbport=i"      =>  \$dbport,
     "password=s"    =>  \$password,
     "database=s"    =>  \$database,
     "fsource=s"     =>  \$fsource,
@@ -24,6 +27,8 @@ print <<HELP;
 
 --user
 --password
+--dbhost (default = lewis2.rnet.missouri.edu)
+--dbport (default = 53307)
 --database (default = 'QuantDisplay')
 --fsource (default = 'GA2')
 --justsummary
@@ -43,12 +48,16 @@ $install_path = "$ENV{HOME}/projects/QuantDisplay" unless ($install_path);
 $verbose = 1 if ($debug);
 $database ||= 'QuantDisplay';
 $fsource ||= 'GA2';
+$dbhost ||= 'lewis2.rnet.missouri.edu';
+$dbport ||= 53307;
 
 if ($debug) {
     print "user = '$user'\npassword = '$password'\ndatabase = '$database'\nfsource = '$fsource'\n";
 }
 
-open(STAT, "$install_path/sql/fdata_stats.mysql $user $password $database $fsource --silent |") or die "can't open fdata_stats.mysql: $!";
+#open(STAT, "$install_path/sql/fdata_stats.mysql $user $password $database $fsource --silent |") or die "can't open fdata_stats.mysql: $!";
+print "$install_path/sql/fdata_stats.mysql $user $password $dbhost $dbport $database $fsource --silent\n" if ($verbose);
+open(STAT, "$install_path/sql/fdata_stats.mysql $user $password $dbhost $dbport $database $fsource --silent |") or die "can't open fdata_stats.mysql: $!";
 my @sql = <STAT>;
 close(STAT) or warn("can't close $install_path/sql/fdata_stats.mysql properly\n");
 
